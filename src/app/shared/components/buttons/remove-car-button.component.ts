6d5107ce07952ec79ed053824b1c826a1876df49
Pavel <verbipo@gmail.com>
2024-05-03 19:16:16 +0200

    feat: styled remove car button

--- src/app/shared/components/buttons/remove-car-button.component.ts
+++ src/app/shared/components/buttons/remove-car-button.component.ts
@@ -1,21 +1,38 @@
 import { Component, computed, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 import { CrudCarService } from '../../services/api/crud-car.service';
 import { CrudWinnerService } from '../../services/api/crud-winner.service';
 import { GetCarsService } from '../../services/api/get-cars.service';
+import { CarDrivingService } from '../../services/car-driving.service';
+import { RaceProcessService } from '../../services/race-process.service';
 
 @Component({
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
+
   selector: 'app-remove-car-button',
   standalone: true,
-  template: `<button
-    (click)="removeCarFromGarage(id)"
-    [disabled]="deletingIsInProgress"
-  >
-    Remove Car
-  </button>`,
+  template: `
+    <button
+      mat-icon-button
+      class="remove-car-button"
+      [disabled]="!isCarInDriving(id) || raceInprogress()"
+      (click)="removeCarFromGarage(id)"
+    >
+      <mat-icon>close</mat-icon>
+    </button>
+  `,
   styles: `
     ::ng-deep app-remove-car-button {
       margin-left: auto;
+    }
+    .remove-car-button {
+      color: white;
+      &:hover {
+        color: red;
+      }
     }
   `,
 })
@@ -33,9 +50,19 @@ export class RemoveCarButtonComponent {
     private crudCarService: CrudCarService,
     private crudWonnerService: CrudWinnerService,
     private getCarsService: GetCarsService,
+    private raceService: RaceProcessService,
+    private carDrivingService: CarDrivingService,
   ) {}
 
   @Input() id!: number;
+
+  raceInprogress = computed(() => {
+    return this.raceService.raceInprogress();
+  });
+
+  isCarInDriving(id: number): boolean {
+    return this.carDrivingService.canTurnCarInDriving(id);
+  }
 
   removeCarFromGarage(id: number) {
     this.deletingIsInProgress = true;
